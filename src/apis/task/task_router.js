f95d7bb8521fe360cac68e0bcf71053a6dcd7186
Khoa <nhukhoacr7@gmail.com>
2025-06-01 15:12:09 +0700

    Mệt hơn nữa

--- src/apis/task/task_router.js
+++ src/apis/task/task_router.js
@@ -4,10 +4,10 @@ import checkrole from '../../middleware/checkmiddleware.js';
 
 const Taskrouter = express.Router();
 
-Taskrouter.post('/create', checkrole(['admin', 'member']), TaskController.createTask);
-Taskrouter.get('/getAll', checkrole(['admin']), TaskController.getAllTask);
-Taskrouter.get('/getById/:id', checkrole(['admin', 'member']), TaskController.getTaskbyId);
-Taskrouter.put('/update/:id', checkrole(['admin']), TaskController.updateTask);
-Taskrouter.delete('/delete/:id', checkrole(['admin']), TaskController.deleteTask);
+Taskrouter.post('/tasks/create', checkrole(['admin', 'member']), TaskController.createTask);
+Taskrouter.get('/tasks/getAll', checkrole(['admin']), TaskController.getAllTask);
+Taskrouter.get('/tasks/getById/:id', checkrole(['admin', 'member']), TaskController.getTaskbyId);
+Taskrouter.put('/tasks/update/:id', checkrole(['admin']), TaskController.updateTask);
+Taskrouter.delete('/tasks/delete/:id', checkrole(['admin']), TaskController.deleteTask);
 export default Taskrouter;
 
